06c3f46007a235369978eab4446f581454c7945c
Christine Oen <oen.christine@gmail.com>
2014-09-22 13:15:12 -0500

    seeded tag names

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Tag.create([
+  {
+    name: "yoga",
+    category: "activity" 
+  },
+  {
+    name: "running",
+    category: "activity" 
+  },
+  {
+    name: "tennis",
+    category: "activity" 
+  },
+  {
+    name: "rock climbing",
+    category: "activity" 
+  },
+  {
+    name: "hiking",
+    category: "activity" 
+  },
+  {
+    name: "biking",
+    category: "activity" 
+  },
+  {
+    name: "music",
+    category: "interest" 
+  },
+  {
+    name: "movies",
+    category: "interest" 
+  },
+  {
+    name: "sports",
+    category: "interest" 
+  },
+  {
+    name: "healthy eating",
+    category: "interest" 
+  },
+  {
+    name: "dogs",
+    category: "interest" 
+  },
+  {
+    name: "cats",
+    category: "interest" 
+  }
+])
+  
