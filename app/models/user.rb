8518fbe826d8d6a495149f6b97a2a4478e5bbef1
Christine Oen <oen.christine@gmail.com>
2014-09-23 18:25:34 -0500

    added photos for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "30x30>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_file_name :photo, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
   has_many :taggings
   has_many :tags, through: :taggings
