c83d3161907acdd73eb8768c8fc72b9a1a78937d
Christine Oen <oen.christine@gmail.com>
2014-09-24 12:00:24 -0500

    profile description items are next to photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -92,6 +92,6 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       # params[:user]
-      params.require(:user).permit(:photo, :firstname, :lastname)
+      params.require(:user).permit(:photo, :firstname, :lastname, :age, :gender, :description)
     end
 end
