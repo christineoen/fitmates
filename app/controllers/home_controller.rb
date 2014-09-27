9fcef5173a3e715b471e93c8a33fa9f1a4375e0b
Christine Oen <oen.christine@gmail.com>
2014-09-27 16:14:40 -0500

    added search radius functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
 
   def index
     @users = User.all
+    if current_user.search_radius && current_user.location_id
+      user_hash = @user.within_radius(@user.search_radius)
+      @users_within_radius = user_hash.map {|user| User.create(user)}
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,29 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 
-  def within_radius
+  def within_radius(search_radius)
     # return list of users who are within the search radius
-    
+    # meter * 0.000621371 = mile
+    # meter = mile / 0.000621371
+
+    radius_in_metres = search_radius / 0.000621371
+    latitude = location.latitude.to_s.to_f
+    longitude = location.longitude.to_s.to_f
+    query_string = ""
+
+    #find locations within the current users search radius
+    locations_within_radius = ActiveRecord::Base.connection.execute("SELECT locations.id, locations.city 
+      FROM locations WHERE earth_box(ll_to_earth(#{latitude}, #{longitude}), #{radius_in_metres}) 
+      @> ll_to_earth(locations.latitude, locations.longitude)")
+
+    #put location_id's into a string to query users and remove trailing 'or' from string
+    locations_within_radius.each do |location|
+      query_string << "location_id = #{location['id']} or "
+    end
+    query_string = query_string[0..-5]
+
+    # find user id's within radius
+    users_within_radius = ActiveRecord::Base.connection.execute("SELECT * from users WHERE #{query_string}")
   end
 
   def similiar_to
